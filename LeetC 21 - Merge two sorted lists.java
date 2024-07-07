778a0ea3b58e325bf6b86a2dbcb68b2a78dad9f0
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-07-06 23:45:56 -0300

    New Submit

---
+++ LeetC 21 - Merge two sorted lists.java
@@ -0,0 +1,19 @@
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        List
+    }
+}
