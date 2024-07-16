8cfb0cbd2287d1add7a2027236f9b12184f794ae
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-07-16 20:53:49 -0300

    Code updated

--- LeetC 35 - Search Insert Position.java
+++ LeetC 35 - Search Insert Position.java
@@ -11,26 +11,19 @@ class Solution {
   public int searchInsert(int[] nums, int target) {
     int l = 0;
     int r = nums.length - 1;
-    int mid = (l + r) / 2;
-    do{
+    while(l <= r){        
+      int mid = l + (r - l) / 2;
+
       if(nums[mid] == target){
         return mid;
       }
-      else if(target > nums[mid]){
-        r = mid;
-        mid = (l + r) / 2;
+      else if(target < nums[mid]){
+        r = mid - 1;
       }
       else{
         l = mid + 1;
-        mid = (l + r) / 2;
       }
-    }while(l != r);
-    if(mid == 0){
-      return (r > target) ? r : ++r;
     }
-    else if (mid == nums.length){
-      return (r > target) ? r : ++r;
-    }
-    return r;
+    return l;       
   }
 }
