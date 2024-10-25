c45a4c651decd5b2fdbb98fb19da52d264523b3e
Juan Nogueira <100308661+nxjuan@users.noreply.github.com>
2024-10-25 10:07:05 -0300

    Leetc 70

---
+++ LeetC - 70 Climbing Stairs.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.climbStairs(4));
+  }
+}
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];        
+    }
+}
